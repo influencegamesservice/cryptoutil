154f5d247f23ea1ece83937eae2e735720241a0a
root <influencegamesservice@gmail.com>
2020-04-13 15:36:43 +0900

    static化とclassmethod化

--- enutil.py
+++ enutil.py
@@ -6,46 +6,56 @@ class Encryption:
     def __init__(self):
         pass
 
-    def __create_key(self):
+    @staticmethod
+    def __create_key():
         return Fernet.generate_key()
     
-    def __save_key(self, key, file_path):
+    @staticmethod
+    def __save_key(key, file_path):
         with open(file_path, 'wb') as f:
             f.write(key)
     
-    def __load_key(self, file_path):
+    @staticmethod
+    def __load_key(file_path):
         with open(file_path, 'rb') as f:
             return f.read()
 
-    def __load_plainstring_to_binary(self, plainstring):
+    @staticmethod
+    def __load_plainstring_to_binary(plainstring):
         return plainstring.encode()
     
-    def __load_encryption(self, en_file):
+    @staticmethod
+    def __load_encryption(en_file):
         with open(en_file, 'rb') as f:
             return f.read()
     
-    def __plaintext2encryption(self, plaintext, key):
+    @staticmethod
+    def __plaintext2encryption(plaintext, key):
         return Fernet(key).encrypt(plaintext)
     
-    def __encryption2plaintext(self, encryption, key):
+    @staticmethod
+    def __encryption2plaintext(encryption, key):
         return Fernet(key).decrypt(encryption)
     
-    def __save_encryption(self, encryption, path):
+    @staticmethod
+    def __save_encryption(encryption, path):
         with open(path, 'wb') as f:
             f.write(encryption)
     
-    def create_encryptionAndkey(self, plainstring, key_save_path, encryption_save_path):
-        key = self.__create_key()
-        self.__save_key(key, key_save_path)
-        bplaintext = self.__load_plainstring_to_binary(plainstring)
-        encryption = self.__plaintext2encryption(bplaintext, key)
-        self.__save_encryption(encryption, encryption_save_path)
+    @classmethod
+    def create_encryptionAndkey(cls, plainstring, key_save_path, encryption_save_path):
+        key = cls.__create_key()
+        cls.__save_key(key, key_save_path)
+        bplaintext = cls.__load_plainstring_to_binary(plainstring)
+        encryption = cls.__plaintext2encryption(bplaintext, key)
+        cls.__save_encryption(encryption, encryption_save_path)
     
-    def load_encryption2plaintext(self, key_load_path, encryption_load_path, return_plain_text_decode=True):
-        bencryption = self.__load_encryption(encryption_load_path)
-        key = self.__load_key(key_load_path)
+    @classmethod
+    def load_encryption2plaintext(cls, key_load_path, encryption_load_path, return_plain_text_with_decode=True):
+        bencryption = cls.__load_encryption(encryption_load_path)
+        key = cls.__load_key(key_load_path)
 
-        if return_plain_text_decode:
-            return self.__encryption2plaintext(bencryption, key).decode()
+        if return_plain_text_with_decode:
+            return cls.__encryption2plaintext(bencryption, key).decode()
         else:
-            return self.__encryption2plaintext(bencryption, key)
+            return cls.__encryption2plaintext(bencryption, key)
